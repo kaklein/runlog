ef8f98c2dd981551a00399df9718f6686918d75b
Katie Klein <katieklein07@gmail.com>
2021-09-24 16:46:32 -0500

    Fix password identification

--- connect.php
+++ connect.php
@@ -10,7 +10,7 @@
             $conn = new PDO("mysql:host=$servername;dbname=$db", $username, $password);
             // set the PDO error mode to exception
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            //echo "Connected successfully";
+            echo "Connected successfully";
         } catch(PDOException $e) {
             echo "Database connection failed: " . $e->getMessage();
         }
