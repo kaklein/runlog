dc425aec662c571fcf1d9a6fe86759845d85e4c7
Katie Klein <katieklein07@gmail.com>
2021-09-24 16:15:30 -0500

    Move code to connect to database to separate file

---
+++ connect.php
@@ -0,0 +1,19 @@
+<?php
+//Connect to MySQL database using PDO driver (code from https://www.w3schools.com/php/php_mysql_connect.asp)
+    function connectToDatabase() {
+        $servername = '127.0.0.1';
+        $username = 'runlog_user';
+        $password = 'run4FUN';
+        $db = 'runlog';
+
+        try {
+            $conn = new PDO("mysql:host=$servername;dbname=$db", $username, $password);
+            // set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            //echo "Connected successfully";
+        } catch(PDOException $e) {
+            echo "Database connection failed: " . $e->getMessage();
+        }
+        return $conn;
+    }
+?>
