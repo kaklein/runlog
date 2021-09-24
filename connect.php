17312cc85de57566dbba4ef37c90048a4c3ddce9
Katie Klein <katieklein07@gmail.com>
2021-09-24 17:01:47 -0500

    Remove echo connection confirmation

--- connect.php
+++ connect.php
@@ -10,7 +10,6 @@
             $conn = new PDO("mysql:host=$servername;dbname=$db", $username, $password);
             // set the PDO error mode to exception
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            echo "Connected successfully";
         } catch(PDOException $e) {
             echo "Database connection failed: " . $e->getMessage();
         }
