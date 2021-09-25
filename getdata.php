195c660bfdd26452abc61539f390aaa33a5b3d5a
Katie Klein <katieklein07@gmail.com>
2021-09-25 09:47:33 -0500

    Add pop-up to confirm delete

--- getdata.php
+++ getdata.php
@@ -5,13 +5,17 @@
         // query database for variables
         $sql = 'SELECT run_date, run_type, distance, time_hours, time_minutes, time_seconds, average_pace, id FROM runs ORDER BY run_date DESC';
         foreach($conn->query($sql) as $row) {
+            // Data population:
             echo "<tr>", "<td>", $row['run_date'], "</td>";
             echo "<td>", $row['run_type'], "</td>";
             echo "<td>", $row['distance'], "</td>";
             echo "<td>", sprintf("%02d", $row['time_hours']), ":", sprintf("%02d", $row['time_minutes']), ":", sprintf("%02d", $row['time_seconds']), "</td>";
             echo "<td>", $row['average_pace'], "</td>";
-            echo "<td><a href='delete.php?id=", $row['id'], "'>Delete</a></td>"; // Delete button - TO DO: create confirm alert instead of immediately deleting
-            echo "<td><a href='edit.php?id=", $row['id'], "'>Edit</a></td></tr>"; // Edit button - TO DO: make it function
+
+            // Delete/edit button creation:
+            echo "<td><form class=delete-form onsubmit='confirmDelete(" . $row['id'] . ")'>"; // Delete button
+                echo "<input type='submit' class='delete-button' value='Delete'></input></form></td>";
+            //echo "<td><a href='edit.php?id=", $row['id'], "'>Edit</a></td></tr>"; // Edit button - TO DO: make it function
         }
     }
 
@@ -24,5 +28,21 @@
             echo "<p>This year: ", $row['year_distance'], " miles</p>";
         }
     }
-
 ?>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script>
+    function confirmDelete(id) {
+        if (confirm("Are you sure you want to delete this activity?")) {
+            $.ajax({
+                url: 'delete.php',
+                type: 'post',
+                data: {deleteId:id},
+                error: function() {
+                        alert("Sorry, we had trouble deleting that activity.")
+                }
+            });
+            location.reload();
+        }
+    }
+</script>
