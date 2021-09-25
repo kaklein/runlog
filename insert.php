1e6fa8448f88f886cb07a59f5acd4be24788ca89
Katie Klein <katieklein07@gmail.com>
2021-09-24 20:54:27 -0500

    Call function to update stats on insert

--- insert.php
+++ insert.php
@@ -5,6 +5,7 @@
     <title>Insert Data</title>
     <?php
         include('connect.php');
+        include('update.php');
     ?>
 </head>
 
@@ -49,50 +50,10 @@
         $sqlInsertRunData = "INSERT INTO runs (user_id, run_date, run_type, distance, time_hours, time_minutes, time_seconds, average_pace)
         VALUES (1, '$run_date', '$run_type', '$distance', '$time_hours', '$time_minutes', '$time_seconds', '$average_pace')"; // user_id is currently hardcoded
 
+        $conn->exec($sqlInsertRunData);
 
-        //TO DO: 
-            //Alter so that these stats get updated whenever runs are added OR deleted/edited
-        // SQL statements to update year/month/week distance
-        $sqlUpdateYearDistance =
-            "UPDATE users
-            INNER JOIN (
-                SELECT user_id, SUM(distance) as year_distance
-                FROM runs
-                WHERE user_id = 1 AND YEAR(run_date) = YEAR(now())
-            ) runs ON users.id = runs.user_id
-            SET users.year_distance = runs.year_distance";
+        updateUserStats($conn);
 
-        $sqlUpdateMonthDistance = 
-            "UPDATE users
-            INNER JOIN (
-                SELECT user_id, SUM(distance) as month_distance
-                FROM runs
-                WHERE user_id = 1 AND MONTH(run_date) = MONTH(now())
-            ) runs ON users.id = runs.user_id
-            SET users.month_distance = runs.month_distance";
-
-        $sqlUpdateWeekDistance =
-            "UPDATE users
-            INNER JOIN (
-                SELECT user_id, SUM(distance) as week_distance
-                FROM runs
-                WHERE user_id = 1
-                    AND CASE
-                            WHEN WEEKDAY(now()) = 6 THEN run_date = CURDATE()
-                            ELSE run_date BETWEEN (now() - INTERVAL WEEKDAY(now()) + 1 DAY) AND now()
-                        END
-                ) runs ON users.id = runs.user_id
-                SET users.week_distance = runs.week_distance";
-
-        // Insert into database
-        try {
-            $conn->exec($sqlInsertRunData);
-            $conn->exec($sqlUpdateYearDistance);
-            $conn->exec($sqlUpdateMonthDistance);
-            $conn->exec($sqlUpdateWeekDistance);
-        } catch(PDOException $e) {
-            echo "Error";
-        }
     ?>
 
 </body>
