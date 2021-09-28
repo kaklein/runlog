dea28fd70bea044dc776a8ee4dd22308eda03f75
Katie Klein <katieklein07@gmail.com>
2021-09-28 09:49:40 -0500

    Restructure format of stats display

--- getdata.php
+++ getdata.php
@@ -3,7 +3,7 @@
     //TO DO: update to only show data for specific user
     function displayRunsTable($conn) {
         // query database for variables
-        $sql = 'SELECT run_date, run_type, distance, time_hours, time_minutes, time_seconds, average_pace, id FROM runs ORDER BY run_date DESC';
+        $sql = 'SELECT run_date, run_type, distance, time_hours, time_minutes, time_seconds, average_pace, id FROM runs WHERE user_id = 1 ORDER BY run_date DESC';
         foreach($conn->query($sql) as $row) {
             // Data population:
             echo "<tr>", "<td>", $row['run_date'], "</td>";
@@ -14,7 +14,7 @@
 
             // Delete/edit button creation:
             echo "<td><form class=delete-form onsubmit='confirmDelete(" . $row['id'] . ")'>"; // Delete button
-                echo "<input type='submit' class='delete-button' value='Delete'></input></form></td>";
+                echo "<input type='submit' class='delete-button button' value='Delete'></input></form></td>";
             //echo "<td><a href='edit.php?id=", $row['id'], "'>Edit</a></td></tr>"; // Edit button - TO DO: make it function
         }
     }
@@ -23,12 +23,22 @@
     function displayUserStats($conn) {
         $sql = 'SELECT week_distance, month_distance, year_distance FROM users WHERE id = 1';
         foreach($conn->query($sql) as $row) {
-            echo "<p>This week: ", $row['week_distance'], " miles</p>";
-            echo "<p>This month: ", $row['month_distance'], " miles</p>";
-            echo "<p>This year: ", $row['year_distance'], " miles</p>";
+            echo "<div class='label-stat-block'><div class='stat-block'>".$row['week_distance']." mi</div>";
+            echo "<div class='label-block'>This Week</div></div>";
+
+            echo "<div class='label-stat-block'><div class='stat-block'>".$row['month_distance']." mi</div>";
+            echo "<div class='label-block'>This Month</div></div>";
+            
+            echo "<div class='label-stat-block'><div class='stat-block'>".$row['year_distance']." mi</div>";
+            echo "<div class='label-block'>This Year</div></div>";
         }
     }
 ?>
+
+
+
+
+
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script>
